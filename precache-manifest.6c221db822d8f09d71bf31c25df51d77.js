0427054c661fbf92250733296efe6a46b330d797
dyuthichand <dyuthichand@gmail.com>
2019-06-28 11:34:06 +0530

    Updates

---
+++ precache-manifest.6c221db822d8f09d71bf31c25df51d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8371c4ab3a9eafc0ced336b9b5005408",
+    "url": "/skill-matrix/index.html"
+  },
+  {
+    "revision": "1d40ff3406c0e2fbf84f",
+    "url": "/skill-matrix/static/css/main.5a77a170.chunk.css"
+  },
+  {
+    "revision": "f408cb46eb110c280118",
+    "url": "/skill-matrix/static/js/2.69883d6b.chunk.js"
+  },
+  {
+    "revision": "1d40ff3406c0e2fbf84f",
+    "url": "/skill-matrix/static/js/main.2231dd16.chunk.js"
+  },
+  {
+    "revision": "e1e4afab53e55ca36c84",
+    "url": "/skill-matrix/static/js/runtime~main.2563de82.js"
+  }
+]);
